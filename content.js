c4b445bbdeb8451914f4deb4b39102f1bb723e1e
vaithschmitz <vaithschmitz@googlemail.com>
2019-09-22 18:25:44 +0100

    svgs

--- content.js
+++ content.js
@@ -1,22 +1,26 @@
-console.log('hello')
-
-let images
-let memes = []
-async function getMemes(){
-    images = Array.from(document.getElementsByTagName('img'))
-    let res = await fetch(' https://meme-api.herokuapp.com/gimme/30')
-    let data = await res.json()
-    await data.memes.forEach(el => memes.push(el.url))
-    console.log('i ran')
-    showMemes()
-}
-
-async function showMemes(){
-    for(let i = 0; i < images.length; i++){
-        images[i].src = memes[i]
+chrome.storage.local.get('enabled', data => {
+    if (data.enabled) {
+        let images
+        let memes = []
+        async function getMemes(){
+            images = Array.from(document.getElementsByTagName('img'))
+            let res = await fetch(' https://meme-api.herokuapp.com/gimme/30')
+            let data = await res.json()
+            await data.memes.forEach(el => memes.push(el.url))
+            console.log('i ran')
+            showMemes()
+        }
+        
+        async function showMemes(){
+            for(let i = 0; i < images.length; i++){
+                images[i].src = memes[i]
+            }
+        }
+        
+        getMemes()
+        
+        setInterval(getMemes, 5000)
+    } else {
     }
-}
+});
 
-getMemes()
-
-setInterval(getMemes, 5000)
--- popup.js
+++ popup.js
@@ -1,13 +1,24 @@
 let memeToggle = document.getElementById('memeToggle');
 
 
-chrome.runtime.onMessage.addListener(
-    function(message, callback) {
-    
-      if (message == “runContentScript”){
-        memeToggle.onclick = chrome.tabs.executeScript({
-            file: 'content.js'
-        });
-      }
-   });
-  
+let enabled = false; //disabled by default
+const myButton = document.getElementById('memeToggle');
+
+chrome.storage.local.get('enabled', data => {
+    enabled = !!data.enabled;
+    myButton.style.backgroundSize = '100%'
+    myButton.style.backgroundRepeat = 'no-repeat'
+    myButton.style.backgroundImage = enabled ? 'url(https://upload.wikimedia.org/wikipedia/ru/7/78/Trollface.svg)' : ''
+    myButton.textContent = enabled ? '' : 'lame';
+    myButton.style.backgroundColor = enabled ? 'yellow' : 'red'
+});
+
+myButton.onclick = () => {
+    enabled = !enabled;
+    myButton.textContent = enabled ? '' : 'lame';
+    myButton.style.backgroundColor = enabled ? 'yellow' : 'red'
+    myButton.style.backgroundImage = enabled ? 'url(https://upload.wikimedia.org/wikipedia/ru/7/78/Trollface.svg)' : ''
+    chrome.storage.local.set({enabled:enabled});
+};
+
+   
