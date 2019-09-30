885ac16f2a40d2d5a578d1ee1dfbecaef2d1f3b4
vaithschmitz <vaithschmitz@googlemail.com>
2019-09-30 09:51:56 +0100

    ship ready

--- content.js
+++ content.js
@@ -1,18 +1,21 @@
 chrome.storage.local.get('enabled', data => {
     if (data.enabled) {
+
         let images
         let memes = []
         async function getMemes(){
-            images = Array.from(document.getElementsByTagName('img'))
-            let res = await fetch(' https://meme-api.herokuapp.com/gimme/30')
+            images = await Array.from(document.getElementsByTagName('img'))
+            let res = await fetch('https://meme-api.herokuapp.com/gimme/30')
             let data = await res.json()
             await data.memes.forEach(el => memes.push(el.url))
+
             showMemes()
         }
         
         async function showMemes(){
             for(let i = 0; i < images.length; i++){
-                images[i].src = memes[i]
+                images[i].style.objectFit = 'contain'
+                images[i].src = memes[i]     
             }
         }
         
--- popup.js
+++ popup.js
@@ -9,14 +9,14 @@ chrome.storage.local.get('enabled', data => {
     myButton.style.backgroundSize = '100%'
     myButton.style.backgroundRepeat = 'no-repeat'
     myButton.style.backgroundImage = enabled ? 'url(https://upload.wikimedia.org/wikipedia/ru/7/78/Trollface.svg)' : ''
-    myButton.textContent = enabled ? '' : 'lame';
-    myButton.style.backgroundColor = enabled ? 'yellow' : 'red'
+    myButton.textContent = enabled ? '' : 'clicc4memez';
+    myButton.style.backgroundColor = enabled ? 'yellow' : 'black'
 });
 
 myButton.onclick = () => {
     enabled = !enabled;
-    myButton.textContent = enabled ? '' : 'lame';
-    myButton.style.backgroundColor = enabled ? 'yellow' : 'red'
+    myButton.textContent = enabled ? '' : 'clicc4memez';
+    myButton.style.backgroundColor = enabled ? 'yellow' : 'black'
     myButton.style.backgroundImage = enabled ? 'url(https://upload.wikimedia.org/wikipedia/ru/7/78/Trollface.svg)' : ''
     chrome.storage.local.set({enabled:enabled});
 };
